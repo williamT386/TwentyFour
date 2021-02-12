193a5c15e7256875f681460cd7de38ac47b7ba00
williamT386 <williamtang.basis@gmail.com>
2021-02-12 09:39:53 -0800

    Added UI for InstructionsActivity.java. Incomplete

--- app/src/main/java/com/example/twentyfour/InstructionsActivity.java
+++ app/src/main/java/com/example/twentyfour/InstructionsActivity.java
@@ -2,13 +2,22 @@ package com.example.twentyfour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class InstructionsActivity extends AppCompatActivity {
+
+    private TextView instructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+        instructions = findViewById(R.id.instructions_text);
+        Context context = this.getApplicationContext();
+        instructions.setText(context.getResources().getString(R.string.instructions_text_1));
+//        instructions.setLayoutParams(new LayoutParams(width, height));
+
     }
 }
